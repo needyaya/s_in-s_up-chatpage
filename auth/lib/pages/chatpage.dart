6818128f96bc36dcba5bd38f82aab91566f53e05
AYA <ayaabouzoul@gmail.com>
2023-04-11 12:27:33 +0000

    chat page can send text

--- auth/lib/pages/chatpage.dart
+++ auth/lib/pages/chatpage.dart
@@ -20,7 +20,7 @@ class _ChatState extends State<chat> {
   List<MyMessageCard> messages = [
     MyMessageCard(
       message: 'ofcc',
-      date: DateTime.now().subtract(const Duration(minutes: 1)),
+      date: DateTime.now().subtract(const Duration(days: 1, minutes: 1)),
       user: true,
     ),
     MyMessageCard(
@@ -35,166 +35,170 @@ class _ChatState extends State<chat> {
     ),
     MyMessageCard(
       message: 'yay',
-      date: DateTime.now().subtract(Duration(minutes: 1)),
+      date: DateTime.now().subtract(Duration(days: 5, minutes: 1)),
       user: false,
     ),
     MyMessageCard(
       message: 'ofcc',
-      date: DateTime.now().subtract(Duration(minutes: 3)),
+      date: DateTime.now().subtract(Duration(days: 9, minutes: 3)),
       user: false,
     ),
   ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          automaticallyImplyLeading: false,
-          backgroundColor: Colors.white,
-          flexibleSpace: SafeArea(
-            child: Container(
-              padding: const EdgeInsets.only(right: 16),
-              child: Row(
-                children: <Widget>[
-                  IconButton(
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                    icon: const Icon(
-                      Icons.arrow_back,
-                      color: Colors.black,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 2,
-                  ),
-                  const CircleAvatar(
-                    maxRadius: 20,
-                  ),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        const Text(
-                          "THE DOORZ",
-                          style: TextStyle(
-                              fontSize: 16, fontWeight: FontWeight.w600),
-                        ),
-                        const SizedBox(
-                          height: 6,
-                        ),
-                        Text(
-                          "Online",
-                          style: TextStyle(
-                              color: Colors.grey.shade600, fontSize: 13),
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        body: Stack(
-          children: <Widget>[
-            Expanded(
-                child: GroupedListView<MyMessageCard, DateTime>(
-              elements: messages,
-              groupBy: (message) => DateTime(
-                  message.date.year, message.date.month, message.date.day),
-              groupHeaderBuilder: (MyMessageCard message) => SizedBox(
-                height: 40,
-                child: Center(
-                  child: Card(
-                    color: Colors.pink,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: Text(
-                        DateFormat.yMMMd().format(message.date).toString(),
-                        style: const TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                ),
-              ),
-              itemBuilder: (context, MyMessageCard message) => Align(
-                alignment:
-                    message.user ? Alignment.centerRight : Alignment.centerLeft,
-                child: Card(
-                  elevation: 8,
-                  child: Padding(
-                    padding: const EdgeInsets.all(12),
-                    child: Text(message.message),
-                  ),
-                ),
-              ),
-            )),
-            Align(
-              alignment: Alignment.bottomLeft,
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            automaticallyImplyLeading: false,
+            backgroundColor: Colors.white,
+            flexibleSpace: SafeArea(
               child: Container(
-                padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
-                height: 60,
-                width: double.infinity,
-                color: Colors.white,
+                padding: const EdgeInsets.only(right: 16),
                 child: Row(
                   children: <Widget>[
-                    GestureDetector(
-                      onTap: () {},
-                      child: Container(
-                        height: 30,
-                        width: 30,
-                        decoration: BoxDecoration(
-                          color: Colors.lightBlue,
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: const Icon(
-                          Icons.add,
-                          color: Colors.white,
-                          size: 20,
-                        ),
+                    IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
                       ),
                     ),
                     const SizedBox(
-                      width: 15,
+                      width: 2,
+                    ),
+                    const CircleAvatar(
+                      maxRadius: 20,
+                    ),
+                    const SizedBox(
+                      width: 12,
                     ),
                     Expanded(
-                      child: TextField(
-                        controller: messagecontroller,
-                        decoration: const InputDecoration(
-                            hintText: " message...",
-                            hintStyle: TextStyle(color: Colors.black54),
-                            border: InputBorder.none),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          const Text(
+                            "THE DOORZ",
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w600),
+                          ),
+                          const SizedBox(
+                            height: 6,
+                          ),
+                          Text(
+                            "Online",
+                            style: TextStyle(
+                                color: Colors.grey.shade600, fontSize: 13),
+                          ),
+                        ],
                       ),
-                    ),
-                    const SizedBox(
-                      width: 15,
-                    ),
-                    FloatingActionButton(
-                      onPressed: () {
-                        print(messagecontroller.text);
-                        String text = messagecontroller.text;
-                        final message = MyMessageCard(
-                            message: text, date: DateTime.now(), user: true);
-                        setState(() => (messages.add(message)));
-                      },
-                      child: Icon(
-                        Icons.send,
-                        color: Colors.white,
-                        size: 18,
-                      ),
-                      backgroundColor: Colors.blue,
-                      elevation: 0,
                     ),
                   ],
                 ),
               ),
             ),
-          ],
-        ));
+          ),
+          body: Stack(
+            children: <Widget>[
+              Expanded(
+                  child: GroupedListView<MyMessageCard, DateTime>(
+                elements: messages,
+                groupBy: (message) => DateTime(
+                    message.date.year, message.date.month, message.date.day),
+                groupHeaderBuilder: (MyMessageCard message) => SizedBox(
+                  height: 40,
+                  child: Center(
+                    child: Card(
+                      color: Colors.pink,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8),
+                        child: Text(
+                          DateFormat.yMMMd().format(message.date).toString(),
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                itemBuilder: (context, MyMessageCard message) => Align(
+                  alignment: message.user
+                      ? Alignment.centerRight
+                      : Alignment.centerLeft,
+                  child: Card(
+                    elevation: 8,
+                    child: Padding(
+                      padding: const EdgeInsets.all(12),
+                      child: Text(message.message),
+                    ),
+                  ),
+                ),
+              )),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Container(
+                  padding: const EdgeInsets.only(left: 10, bottom: 10, top: 10),
+                  height: 60,
+                  width: double.infinity,
+                  color: Colors.white,
+                  child: Row(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {},
+                        child: Container(
+                          height: 30,
+                          width: 30,
+                          decoration: BoxDecoration(
+                            color: Colors.lightBlue,
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: const Icon(
+                            Icons.add,
+                            color: Colors.white,
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      Expanded(
+                        child: TextField(
+                          controller: messagecontroller,
+                          decoration: const InputDecoration(
+                              hintText: " message...",
+                              hintStyle: TextStyle(color: Colors.black54),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 15,
+                      ),
+                      FloatingActionButton(
+                        onPressed: () {
+                          print(messagecontroller.text);
+                          String text = messagecontroller.text;
+                          final message = MyMessageCard(
+                              message: text, date: DateTime.now(), user: true);
+                          setState(() => (messages.add(message)));
+                          messagecontroller.clear();
+                        },
+                        child: Icon(
+                          Icons.send,
+                          color: Colors.white,
+                          size: 18,
+                        ),
+                        backgroundColor: Colors.blue,
+                        elevation: 0,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
+    );
   }
 }
--- auth/lib/pages/home.dart
+++ auth/lib/pages/home.dart
@@ -7,144 +7,151 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.pink[200],
-        title: const Text(
-          'ayas world ',
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.pink[200],
+          title: const Text(
+            'ayas world ',
+          ),
         ),
-      ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <
-            Widget>[
-          SafeArea(
-            child: Padding(
-              padding: EdgeInsets.only(left: 16, right: 16, top: 10),
-              child: Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    const Text(
-                      "Conversations",
-                      style:
-                          TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-                    ),
-                    Container(
-                      padding:
-                          EdgeInsets.only(left: 8, right: 8, top: 2, bottom: 2),
-                      height: 30,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(30),
-                        color: Colors.pink[50],
-                      ),
+        body: SingleChildScrollView(
+          physics: const BouncingScrollPhysics(),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SafeArea(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 16, right: 16, top: 10),
+                    child: Container(
                       child: Row(
-                        children: const <Widget>[
-                          Icon(
-                            Icons.add,
-                            color: Colors.pink,
-                            size: 20,
-                          ),
-                          SizedBox(
-                            width: 2,
-                          ),
-                          Text(
-                            "Add New",
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          const Text(
+                            "Conversations",
                             style: TextStyle(
-                                fontSize: 14, fontWeight: FontWeight.bold),
+                                fontSize: 32, fontWeight: FontWeight.bold),
                           ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 50,
-          ),
-          SafeArea(
-              child: GestureDetector(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => chat(),
-                  ));
-            },
-            child: Container(
-              padding:
-                  EdgeInsets.only(left: 16, right: 16, top: 10, bottom: 10),
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Row(
-                      children: <Widget>[
-                        CircleAvatar(
-                          backgroundImage: NetworkImage("images/mypicture.jpg"),
-                          maxRadius: 30,
-                        ),
-                        SizedBox(
-                          width: 16,
-                        ),
-                        Expanded(
-                          child: Container(
-                            color: Colors.transparent,
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Text(
-                                  'THE DOORZ',
-                                  style: TextStyle(fontSize: 16),
+                          Container(
+                            padding: EdgeInsets.only(
+                                left: 8, right: 8, top: 2, bottom: 2),
+                            height: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(30),
+                              color: Colors.pink[50],
+                            ),
+                            child: Row(
+                              children: const <Widget>[
+                                Icon(
+                                  Icons.add,
+                                  color: Colors.pink,
+                                  size: 20,
                                 ),
                                 SizedBox(
-                                  height: 6,
+                                  width: 2,
                                 ),
                                 Text(
-                                  'hello',
+                                  "Add New",
                                   style: TextStyle(
-                                      fontSize: 13,
-                                      color: Colors.grey.shade600,
+                                      fontSize: 14,
                                       fontWeight: FontWeight.bold),
                                 ),
                               ],
                             ),
                           ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                SafeArea(
+                    child: GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => chat(),
+                        ));
+                  },
+                  child: Container(
+                    padding: EdgeInsets.only(
+                        left: 16, right: 16, top: 10, bottom: 10),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Row(
+                            children: <Widget>[
+                              CircleAvatar(
+                                backgroundImage:
+                                    NetworkImage("assets/images/mypicture.jpg"),
+                                maxRadius: 30,
+                              ),
+                              SizedBox(
+                                width: 16,
+                              ),
+                              Expanded(
+                                child: Container(
+                                  color: Colors.transparent,
+                                  child: Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                      Text(
+                                        'THE DOORZ',
+                                        style: TextStyle(fontSize: 16),
+                                      ),
+                                      SizedBox(
+                                        height: 6,
+                                      ),
+                                      Text(
+                                        'hello',
+                                        style: TextStyle(
+                                            fontSize: 13,
+                                            color: Colors.grey.shade600,
+                                            fontWeight: FontWeight.bold),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Text(
+                          DateTime.now().toString(),
+                          style: TextStyle(
+                              fontSize: 12, fontWeight: FontWeight.bold),
                         ),
                       ],
                     ),
                   ),
-                  Text(
-                    DateTime.now().toString(),
-                    style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-                  ),
-                ],
-              ),
+                )),
+              ]),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Colors.pink[200],
+          unselectedItemColor: Colors.grey.shade600,
+          selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
+          unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+          type: BottomNavigationBarType.fixed,
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.message),
+              label: "Chats",
             ),
-          )),
-        ]),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Colors.pink[200],
-        unselectedItemColor: Colors.grey.shade600,
-        selectedLabelStyle: const TextStyle(fontWeight: FontWeight.w600),
-        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
-        type: BottomNavigationBarType.fixed,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.message),
-            label: "Chats",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.photo),
-            label: "pictures",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.account_box),
-            label: "Profile",
-          ),
-        ],
+            BottomNavigationBarItem(
+              icon: Icon(Icons.photo),
+              label: "pictures",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.account_box),
+              label: "Profile",
+            ),
+          ],
+        ),
       ),
     );
 
